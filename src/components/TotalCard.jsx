693fd17422c154bd4d56c140c09001a273d7b1b4
Shivam Yadav <154537760+shivamy009@users.noreply.github.com>
2024-08-26 08:01:40 +0530

    Update TotalCard.jsx

--- src/components/TotalCard.jsx
+++ src/components/TotalCard.jsx
@@ -10,9 +10,9 @@ const TotalCard = () => {
   return (
     <div className=' ml-10 w-full grid grid-cols-3   '>
    {
-    Medicine.map(data=>{
+    Medicine.map((data,key)=>{
       return(
-       <Card uppertag={data.uppertag} image={data.image} company={data.company} contiontration1={data.contiontration1} contiontration2={data.contiontration2} price={data.price} programm={data.programm} description={data.description} kutvivar={data.kutvivar} curvebutton={data.curvebutton}/>
+       <Card key={key} uppertag={data.uppertag} image={data.image} company={data.company} contiontration1={data.contiontration1} contiontration2={data.contiontration2} price={data.price} programm={data.programm} description={data.description} kutvivar={data.kutvivar} curvebutton={data.curvebutton}/>
       )
     })
    }
